e3268938a94f29f774e128166d38f3ad5fa9e288
frido aka fmaurer <fmaurer42@gmail.com>
2022-07-30 11:42:36 +0200

    add header to algorithm

--- rush01_algorithm.c
+++ rush01_algorithm.c
@@ -6,9 +6,11 @@
 /*   By: fmaurer <fmaurer42@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/30 11:33:32 by fmaurer           #+#    #+#             */
-/*   Updated: 2022/07/30 11:34:58 by fmaurer          ###   ########.fr       */
+/*   Updated: 2022/07/30 11:42:10 by fmaurer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <rush01.h>
 
 void rush01_algorithm(int *views)
 {
