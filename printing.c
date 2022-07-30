ec1c6225fd25a157b8381f5b87c2e528cf3d9130
frido aka fmaurer <fmaurer42@gmail.com>
2022-07-30 15:52:41 +0200

    ft_print.c -> printing.c & add squareness

--- printing.c
+++ printing.c
@@ -6,7 +6,7 @@
 /*   By: fmaurer <fmaurer42@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/29 23:08:43 by fmaurer           #+#    #+#             */
-/*   Updated: 2022/07/30 08:40:57 by fmaurer          ###   ########.fr       */
+/*   Updated: 2022/07/30 15:50:32 by fmaurer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,28 @@ void	ft_print(char *str)
 
 	bytes = ft_strlen(str);
 	write(1, str, bytes);
+}
+
+void	print_square(int **square, int n)
+{
+	int	j;
+	int	k;
+	char	c;
+
+	j = 0;
+	k = 0;
+
+	while (j < n)
+	{
+		k = 0;
+		while (k < n)
+		{
+			c = '0' + square[j][k];
+			write(1, &c, 1);
+			write(1, " ", 1);
+			k++;
+		}
+		write(1, "\n", 1);
+		j++;
+	}
 }
