7098d11df76d6bd2957577d340544ce5f83f4468
frido aka fmaurer <fmaurer42@gmail.com>
2022-07-30 14:35:42 +0200

    debug-output: outsource and add to header-file

--- debug-rush01.c
+++ debug-rush01.c
@@ -1,24 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   rush01.h                                           :+:      :+:    :+:   */
+/*   debug-rush01.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fmaurer <fmaurer42@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/30 08:39:14 by fmaurer           #+#    #+#             */
-/*   Updated: 2022/07/30 14:30:19 by fmaurer          ###   ########.fr       */
+/*   Created: 2022/07/30 14:28:00 by fmaurer           #+#    #+#             */
+/*   Updated: 2022/07/30 14:31:07 by fmaurer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "rush01.h"
 
-int	ft_strlen(char *str);
+// FIXME DEBUG remove again !!!
+#include <stdio.h>
 
-void	ft_print(char *str);
+void debug_output(int cmdline_nums, char ** number_strings, int *num_array)
+{
+	int cnt;
 
-char	**ft_split_cnt(char *str, int *words);
-
-int	*strs_to_1to4(char **strs, int n);
-
-void debug_output(int cmdline_nums, char ** number_strings, int *num_array);
-
-
+	cnt = 0;
+	while (cnt < cmdline_nums)
+	{
+		ft_print("str: ");
+		ft_print(number_strings[cnt]);
+		ft_print("\n");
+		cnt++;
+	}
+	for(int i = 0; i < cmdline_nums; i++)
+		printf("num_array[%d] = %d\n", i, num_array[i]);
+	// end DEBUG output
+}
