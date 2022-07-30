f19e10ac8eb281c071507ae0b156b12a841e5b9d
frido aka fmaurer <fmaurer42@gmail.com>
2022-07-30 09:00:54 +0200

    strs_to_1to4.c: add this function

---
+++ strs_to_1to4.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   strs_to_1to4.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fmaurer <fmaurer42@gmail.com>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/30 09:00:42 by fmaurer           #+#    #+#             */
+/*   Updated: 2022/07/30 09:00:45 by fmaurer          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "rush01.h"
+
+int	*strs_to_1to4(char **strs, int n)
+{
+	int	*mem;
+	int	j;
+
+	mem = (int *) malloc(n * sizeof (int));
+	if (mem == NULL)
+		return (NULL);
+	j = -1;
+	while (++j < n)
+	{
+		if (ft_strlen(strs[j]) != 1 && !('1' <= strs[j][0] && \
+				strs[j][0] <= '4'))
+			return (NULL);
+		mem[j] = strs[j][0] - '0';
+	}
+	return (mem);
+}
