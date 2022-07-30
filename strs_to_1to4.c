eeeb800a2f9bfd1f8a17102a5b77e9439738c486
frido aka fmaurer <fmaurer42@gmail.com>
2022-07-30 14:34:44 +0200

    strs_to_1to4: fix error condition

--- strs_to_1to4.c
+++ strs_to_1to4.c
@@ -6,9 +6,12 @@
 /*   By: fmaurer <fmaurer42@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/30 09:00:42 by fmaurer           #+#    #+#             */
-/*   Updated: 2022/07/30 09:00:45 by fmaurer          ###   ########.fr       */
+/*   Updated: 2022/07/30 14:25:47 by fmaurer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+// DEBUG
+#include <stdio.h>
 
 #include <stdlib.h>
 #include "rush01.h"
@@ -24,9 +27,9 @@ int	*strs_to_1to4(char **strs, int n)
 	j = -1;
 	while (++j < n)
 	{
-		if (ft_strlen(strs[j]) != 1 && !('1' <= strs[j][0] && \
+		if (ft_strlen(strs[j]) != 1 || !('1' <= strs[j][0] && \
 				strs[j][0] <= '4'))
-			return (NULL);
+				return (NULL);
 		mem[j] = strs[j][0] - '0';
 	}
 	return (mem);
